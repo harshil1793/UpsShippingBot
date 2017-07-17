3e5c20e60ccfcb704664d94b01fce140317c1deb
harshil1793 <hmoradia@nyit.edu>
2017-07-17 15:34:01 -0400

    Update prompts.js

--- prompts.js
+++ prompts.js
@@ -6,5 +6,7 @@ module.exports= {
     shipmentSizeMessage: 'Please enter the size of the package',
     pickupAddressMessage: 'Where will we pick up your package from?',
     pickupTimeMessage: 'What time do you want your package to be picked up?',
-    shipAgainMessage: 'Would you like to ship another package?'
+    shipAgainMessage: 'Would you like to ship another package?',
+    AppId: '78ef7be6-b59e-44d7-a7be-bbac7dee4fb1',
+    AppPassword: 'jZyHFYrvd6atkAE1ofnPFs4'
 }
